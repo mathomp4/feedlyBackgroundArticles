6d006f6571cf10f68def05a634a093b495561e0c
Matthew Thompson <matthew.thompson@nasa.gov>
2025-02-11 18:53:19 -0500

    Fixes for latest Feedly

--- FeedlyBackground/content.js
+++ FeedlyBackground/content.js
@@ -1,33 +1,38 @@
-
 var hotkey;
 chrome.storage.sync.get({ hotkey: ';' }, function (items) {
-    hotkey = items.hotkey
+    hotkey = items.hotkey;
+    window.addEventListener('keydown', keyListener, false);
 });
 
 var keyListener = function (e) {
     if (!e.repeat && e.key === hotkey) {
-        // Find the selected frame's title link
-        var targetURL = null
-        var inlinedFrame = $('.inlineFrame--selected').find('a.entryTitle')
-        var openedArticle = $("[aria-label='Opened Article']").find('a.entryTitle')
-        var selectedArticle = $('.entry--selected').find('a.entry__title')
-        var articleTitle = $(".Article__title")
-        if (inlinedFrame.length != 0) {
-            targetURL = inlinedFrame.attr('href')
-        } else if (openedArticle.length != 0) {
-            targetURL = openedArticle.attr('href')
-        } else if (selectedArticle.length != 0) {
-            targetURL = selectedArticle.attr('href')
-        } else if (articleTitle.length != 0) {
-            targetURL = articleTitle.attr('href')
+        // Try different approaches to find the article link
+        var targetURL = null;
+
+        // Attempt to find the article link in different known structures
+        var inlinedArticle = $("a.ArticleTitle--inlined-article");
+        var generalArticle = $("a.ArticleTitle");  // More general class match
+        var selectedEntry = $(".entry--selected a.entry__title");
+        var openArticle = $("[aria-label='Opened Article'] a.entryTitle");
+
+        // Try different options in order of priority
+        if (inlinedArticle.length > 0) {
+            targetURL = inlinedArticle.attr('href');
+        } else if (generalArticle.length > 0) {
+            targetURL = generalArticle.attr('href');
+        } else if (selectedEntry.length > 0) {
+            targetURL = selectedEntry.attr('href');
+        } else if (openArticle.length > 0) {
+            targetURL = openArticle.attr('href');
         } else {
-            console.log("No target URLs found")
+            console.log("No target URLs found");
         }
 
-        if (targetURL != null) {
+        // Open the link if found
+        if (targetURL) {
+            console.log("Opening URL:", targetURL);
             chrome.runtime.sendMessage({ url: targetURL });
         }
     }
-}
+};
 
-window.addEventListener('keydown', keyListener, false);
